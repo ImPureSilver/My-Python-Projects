af550bf53593aa71241e1376bc3efb1122388d77
ImPureSilver <41513961+ImPureSilver@users.noreply.github.com>
2018-12-29 11:56:50 -0500

    Add files via upload
    This is my first dictionary type of program

---
+++ My Dictionary/app.py
@@ -0,0 +1,27 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+def word(w):
+    w = w.lower()
+    if w in data:
+	    return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes or N if no: " % get_close_matches(w, data.keys())[0])
+        if yn == "Y" or "y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N" or "n":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
+    else:
+        return "That word is not in the dictionary, please double check it."
+
+user = input("Enter word: ")
+output = word(user)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
